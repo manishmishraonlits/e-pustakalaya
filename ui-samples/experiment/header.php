12acaaa6efbeee605a4a360f2b9f06e541b29b61
Sachin Kumar <mjsachin03@gmail.com>
2024-07-22 15:10:20 +0530

    Create and upload some UI samples Fixes #2

---
+++ ui-samples/experiment/header.php
@@ -0,0 +1,9 @@
+<header>
+    <img src="image.jpg" alt="ePustakalaya" class="logo">
+    <a href="#">Home</a>
+    <a href="#">Books</a>
+    <a href="#">Member </a>
+    <a href="#">Circulation</a>
+    <a href="#">Notification</a>
+    <button>Log Out</button>
+</header>
---
+++ ui-samples/experiment/home_scr.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" class="css">
+    <title>Home Screen</title>
+</head>
+
+<body>
+    <header>
+        <img src="image.jpg" alt="ePustakalaya" class="logo" width="40px">
+        <a href="#">Admin</a>
+        <a href="#">Staff</a>
+        <a href="#">Member</a>
+    </header>
+    <div>
+
+    </div>
+    <?php include "footer.php"; ?>
+</body>
+
+</html>
